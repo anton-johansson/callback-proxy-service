d3445024f44ebcd1c1c86300edcc94cf7069f4fa
Anton Johansson <antoon.johansson@gmail.com>
2019-02-26 00:03:35 +0100

    Properly build tag

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
 docker tag $DOCKER_USERNAME/callback-proxy-service:latest $DOCKER_USERNAME/callback-proxy-service:$TRAVIS_TAG
 docker push $DOCKER_USERNAME/callback-proxy-service:$TRAVIS_TAG
