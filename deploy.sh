669797a3f502fe782a7a06bfdc0257c83a39ad9f
Anton Johansson <antoon.johansson@gmail.com>
2019-02-25 23:55:17 +0100

    Deploy Docker images on tags

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+docker tag $DOCKER_USERNAME/callback-proxy-service:latest $DOCKER_USERNAME/callback-proxy-service:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/callback-proxy-service:$TRAVIS_TAG
