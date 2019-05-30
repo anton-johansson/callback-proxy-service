bcb97c5e95d5b1348930c34b5c42c72ceb335581
Anton Johansson <antoon.johansson@gmail.com>
2019-05-30 18:31:07 +0200

    Fix bad promise

--- server/auth/static/index.js
+++ server/auth/static/index.js
@@ -1,16 +1,16 @@
 const config = require('../../config')().auth.static;
 
 module.exports = (username, password) => {
-    const user = config.users.find(user => username === user.username && password === user.password);
-    if (user) {
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        const user = config.users.find(user => username === user.username && password === user.password);
+        if (user) {
             resolve({
                 username: user.username,
                 name: user.name,
                 email: user.email
             });
-        });
-    } else {
-        reject(new Error('Could not find user'));
-    }
+        } else {
+            reject(new Error('Could not find user'));
+        }
+    });
 }
